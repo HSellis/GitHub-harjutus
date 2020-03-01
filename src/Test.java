a1c552002259d3ec3973a695721b673ef63b0333
Juhantam <you@example.com>
2020-03-01 13:09:13 +0200

    change to friend still here

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Friendoritos were hereitos");
+        System.out.println("Friend still here");
     }
 }
